f0eeb216f03f390b4686408d4a8933e8ae2ba7ce
d9 <6982205+d953i@users.noreply.github.com>
2019-08-30 10:31:34 -0400

    Add proc to read regulator temperatures

--- scripts/fk33_jtagaxi.tcl
+++ scripts/fk33_jtagaxi.tcl
@@ -1,116 +1,66 @@
 #Assuming I2C controller base address is 0x0000_9000 and FK33 LED's connected to its GPO port.
 
-proc fk33_setup_sens {} {
-	run_hw_axi [create_hw_axi_txn -force c0 [get_hw_axis hw_axi_1] -address 0x00009040 -data 0x0000000A -type write]
-        run_hw_axi [create_hw_axi_txn -force c1 [get_hw_axis hw_axi_1] -address 0x00009120 -data 0x00000002 -type write]
-        run_hw_axi [create_hw_axi_txn -force c2 [get_hw_axis hw_axi_1] -address 0x00009100 -data 0x00000002 -type write]
-        #run_hw_axi [create_hw_axi_txn -force c3 [get_hw_axis hw_axi_1] -address 0x00009100 -data 0x00000001 -type write]
-
-	create_hw_axi_txn -force c4 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000130 -type write
-	create_hw_axi_txn -force c5 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000005 -type write
-	create_hw_axi_txn -force c6 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000131 -type write
-        create_hw_axi_txn -force c7 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000200 -type write
-        create_hw_axi_txn -force e0 [get_hw_axis hw_axi_1] -address 0x00009100 -data 0x00000001 -type write
-	#create_hw_axi_txn -force c7 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000202 -type write
-
-        create_hw_axi_txn -force c8 [get_hw_axis hw_axi_1] -address 0x0000910C -type read
-        create_hw_axi_txn -force c9 [get_hw_axis hw_axi_1] -address 0x0000910C -type read
-        #create_hw_axi_txn -force s [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000200 -type write
-        #create_hw_axi_txn -force c10 [get_hw_axis hw_axi_1] -address 0x0000910C -type read
-        #create_hw_axi_txn -force c11 [get_hw_axis hw_axi_1] -address 0x0000910C -type read
-
-        #run_hw_axi c0 c1 c2 c3 c4 c5 c6 c7 c8 c9 
-        run_hw_axi c4 c5 c6 c7 e0 c8 c9 
-#s
-	#  c8 c9
-        puts [get_property DATA [get_hw_axi_txn c8]]
-        puts [get_property DATA [get_hw_axi_txn c9]]
-
-        #create_hw_axi_txn -quiet -force c5 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000119 -type write
-	#create_hw_axi_txn -quiet -force c6 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000202 -type write
-
-        #create_hw_axi_txn -quiet -force c7 [get_hw_axis hw_axi_1] -address 0x0000910C -data 0x00000202 -type read
-        #create_hw_axi_txn -quiet -force c8 [get_hw_axis hw_axi_1] -address 0x0000910C -data 0x00000202 -type read
-	
-	#run_hw_axi -quiet c0 c1 c2 c3 c4 c5 c6 c7 c8
-        #run_hw_axi c0 c1 c2 c3 c4 c5 c6 c7 c8
-
-#	run_hw_axi [create_hw_axi_txn -force c [get_hw_axis hw_axi_1] -address 0x0000910C -type read]
-#       run_hw_axi [create_hw_axi_txn -force c [get_hw_axis hw_axi_1] -address 0x00009104 -type read]
-#	puts [get_property DATA [get_hw_axi_txn c]]
+proc fk33_regulator_temps {} {
+    fk33_read_i2c_temp 0x18
+    fk33_read_i2c_temp 0x19
+    fk33_read_i2c_temp 0x1F
 }
 
-proc fk33_check_rx_fifo {} {
+proc fk33_read_i2c_temp {i2c_addr} {
+    set addr [format 0x%02x [expr ($i2c_addr << 1)]]
+    create_hw_axi_txn -quiet -force c0 [get_hw_axis hw_axi_1] -address 0x00009040 -data 0x0000000A -type write
+    create_hw_axi_txn -quiet -force c1 [get_hw_axis hw_axi_1] -address 0x00009120 -data 0x00000002 -type write
+    create_hw_axi_txn -quiet -force c2 [get_hw_axis hw_axi_1] -address 0x00009100 -data 0x00000000 -type write
+    create_hw_axi_txn -quiet -force c3 [get_hw_axis hw_axi_1] -address 0x00009108 -data [format 0x%08x [expr $addr | 0x100]] -type write
+    create_hw_axi_txn -quiet -force c4 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000005 -type write
+    create_hw_axi_txn -quiet -force c5 [get_hw_axis hw_axi_1] -address 0x00009108 -data [format 0x%08x [expr $addr | 0x101]] -type write
+    create_hw_axi_txn -quiet -force c6 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000202 -type write
+    create_hw_axi_txn -quiet -force e0 [get_hw_axis hw_axi_1] -address 0x00009100 -data 0x00000001 -type write
+    create_hw_axi_txn -quiet -force r0 [get_hw_axis hw_axi_1] -address 0x0000910C -type read
+    create_hw_axi_txn -quiet -force r1 [get_hw_axis hw_axi_1] -address 0x0000910C -type read
+    run_hw_axi -quiet c0 c1 c2 c3 c4 c5 c6 e0 r0 r1 c0
 
-run_hw_axi [create_hw_axi_txn -force c [get_hw_axis hw_axi_1] -address 0x00009104 -type read]
-puts [get_property DATA [get_hw_axi_txn c]]
-#puts [expr 0x1 & [expr 0x[get_property DATA [get_hw_axi_txn c]] >> 6]]
+    set UB [format 0x%02x [expr 0x[get_property DATA [get_hw_axi_txn r0]]]]
+    set LB [format 0x%02x [expr 0x[get_property DATA [get_hw_axi_txn r1]]]]
+    #puts [format "I2C addr 0x%02x read: 0x%02x 0x%02x" $i2c_addr $UB $LB]
 
-run_hw_axi [create_hw_axi_txn -force o [get_hw_axis hw_axi_1] -address 0x00009118 -type read]
-puts [get_property DATA [get_hw_axi_txn o]]
-
+    set UB [format %.2f [expr $UB & 0x1F]]
+    set LB [format %.2f [expr $LB]]
+    puts [format "I2C addr 0x%02x temp: %.2f degree" $i2c_addr [expr $UB * 16 + $LB / 16]]
 }
-
-proc fk33_read_rx_fifo {} {
-
-run_hw_axi [create_hw_axi_txn -force r0 [get_hw_axis hw_axi_1] -address 0x0000910C -type read]
-puts [get_property DATA [get_hw_axi_txn r0]]
-
-#run_hw_axi [create_hw_axi_txn -force r1 [get_hw_axis hw_axi_1] -address 0x0000910C -type read]
-#puts [get_property DATA [get_hw_axi_txn r1]]
-
-#run_hw_axi [create_hw_axi_txn -force s [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000200 -type write]
-
-}
-
-
-proc fk33_read_temp {} {
-	#create_hw_axi_txn -quiet -force c0 [get_hw_axis hw_axi_1] -address 0x00009040 -data 0x0000000A -type write
-        #create_hw_axi_txn -quiet -force c1 [get_hw_axis hw_axi_1] -address 0x00009120 -data 0x0000000F -type write
-        #create_hw_axi_txn -quiet -force c2 [get_hw_axis hw_axi_1] -address 0x00009100 -data 0x00000001 -type write
-	#create_hw_axi_txn -quiet -force c3 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000118 -type write
-	#create_hw_axi_txn -quiet -force c4 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000205 -type write
-
-        create_hw_axi_txn -force s0 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000119 -type write
-	create_hw_axi_txn -force s1 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000204 -type write
-        create_hw_axi_txn -force s2 [get_hw_axis hw_axi_1] -address 0x0000910C -type read
-        create_hw_axi_txn -force s3 [get_hw_axis hw_axi_1] -address 0x0000910C -type read
-        run_hw_axi s0 s1 s2 s3
-}
-
 
 #To set FK33 VCCINT to 0.85V write to I2C address 0x2C, to register 0x00, value 0x44.
 proc fk33_set_vccint {} {
-	create_hw_axi_txn -quiet -force wr_tx0 [get_hw_axis hw_axi_1] -address 0x00009040 -data 0x0000000A -type write
-	create_hw_axi_txn -quiet -force wr_tx1 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000158 -type write
-	create_hw_axi_txn -quiet -force wr_tx2 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000000 -type write
-	create_hw_axi_txn -quiet -force wr_tx3 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000244 -type write
-	create_hw_axi_txn -quiet -force wr_tx4 [get_hw_axis hw_axi_1] -address 0x00009100 -data 0x00000001 -type write
-	run_hw_axi -quiet wr_tx0 wr_tx1 wr_tx2 wr_tx3 wr_tx4
+    create_hw_axi_txn -quiet -force wr_tx0 [get_hw_axis hw_axi_1] -address 0x00009040 -data 0x0000000A -type write
+    create_hw_axi_txn -quiet -force wr_tx1 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000158 -type write
+    create_hw_axi_txn -quiet -force wr_tx2 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000000 -type write
+    create_hw_axi_txn -quiet -force wr_tx3 [get_hw_axis hw_axi_1] -address 0x00009108 -data 0x00000244 -type write
+    create_hw_axi_txn -quiet -force wr_tx4 [get_hw_axis hw_axi_1] -address 0x00009100 -data 0x00000001 -type write
+    run_hw_axi -quiet wr_tx0 wr_tx1 wr_tx2 wr_tx3 wr_tx4
 }
 
 #To set FK33 user LED's(4Green + 1RGB) write to I2C controller GPO register (0x124) 32bit value.
 proc fk33_set_led {value} {
-	create_hw_axi_txn wr_tx_led [get_hw_axis hw_axi_1] -address 0x00009124 -data [format 0x%08x [expr $value]] -type write -force
-	run_hw_axi -quiet wr_tx_led
+    create_hw_axi_txn wr_tx_led [get_hw_axis hw_axi_1] -address 0x00009124 -data [format 0x%08x [expr $value]] -type write -force
+    run_hw_axi -quiet wr_tx_led
 }
 
 proc fk33_set_vccint_64 {} {
-	create_hw_axi_txn -quiet -force wr_tx0_64 [get_hw_axis hw_axi_1] -address 0x200000040 -data 0x000000000000000A -type write
-	create_hw_axi_txn -quiet -force wr_tx1_64 [get_hw_axis hw_axi_1] -address 0x200000108 -data 0x0000000000000158 -type write
-	create_hw_axi_txn -quiet -force wr_tx2_64 [get_hw_axis hw_axi_1] -address 0x200000108 -data 0x0000000000000000 -type write
-	create_hw_axi_txn -quiet -force wr_tx3_64 [get_hw_axis hw_axi_1] -address 0x200000108 -data 0x0000000000000244 -type write
-	create_hw_axi_txn -quiet -force wr_tx4_64 [get_hw_axis hw_axi_1] -address 0x200000100 -data 0x0000000000000001 -type write
-	run_hw_axi -quiet wr_tx0_64 wr_tx1_64 wr_tx2_64 wr_tx3_64 wr_tx4_64
+    create_hw_axi_txn -quiet -force wr_tx0_64 [get_hw_axis hw_axi_1] -address 0x200000040 -data 0x000000000000000A -type write
+    create_hw_axi_txn -quiet -force wr_tx1_64 [get_hw_axis hw_axi_1] -address 0x200000108 -data 0x0000000000000158 -type write
+    create_hw_axi_txn -quiet -force wr_tx2_64 [get_hw_axis hw_axi_1] -address 0x200000108 -data 0x0000000000000000 -type write
+    create_hw_axi_txn -quiet -force wr_tx3_64 [get_hw_axis hw_axi_1] -address 0x200000108 -data 0x0000000000000244 -type write
+    create_hw_axi_txn -quiet -force wr_tx4_64 [get_hw_axis hw_axi_1] -address 0x200000100 -data 0x0000000000000001 -type write
+    run_hw_axi -quiet wr_tx0_64 wr_tx1_64 wr_tx2_64 wr_tx3_64 wr_tx4_64
 }
 
 proc fk33_set_led_64 {value} {
-	create_hw_axi_txn wr_tx_led_64 [get_hw_axis hw_axi_1] -address 0x200000124 -data [format 0x%08x00000000 [expr $value]] -type write -force
-	run_hw_axi -quiet wr_tx_led_64
+    create_hw_axi_txn wr_tx_led_64 [get_hw_axis hw_axi_1] -address 0x200000124 -data [format 0x%08x00000000 [expr $value]] -type write -force
+    run_hw_axi -quiet wr_tx_led_64
 }
 
 #JTAG to AXI tcl procedure to write 256bit data to 64bit address.
-proc write_mem256 {addr value} {
+proc axi256_write {addr value} {
     create_hw_axi_txn -force w0 [get_hw_axis hw_axi_1] -address [format 0x%016lx [expr $addr +  0]] -data [format 0x%016lx [expr ($value >>   0) & 0xFFFFFFFFFFFFFFFFFFFFFFFF]] -type write
     create_hw_axi_txn -force w1 [get_hw_axis hw_axi_1] -address [format 0x%016lx [expr $addr +  8]] -data [format 0x%016lx [expr ($value >>  64) & 0xFFFFFFFFFFFFFFFFFFFFFFFF]] -type write
     create_hw_axi_txn -force w2 [get_hw_axis hw_axi_1] -address [format 0x%016lx [expr $addr + 16]] -data [format 0x%016lx [expr ($value >> 128) & 0xFFFFFFFFFFFFFFFFFFFFFFFF]] -type write
@@ -119,7 +69,7 @@ proc write_mem256 {addr value} {
 }
 
 #JTAG to AXI tcl procedure to read 256bit data from 64bit address.
-proc read_mem256 {addr} {
+proc axi256_read {addr} {
     create_hw_axi_txn -force r0 [get_hw_axis hw_axi_1] -address [format 0x%016lx [expr $addr +  0]] -type read
     create_hw_axi_txn -force r1 [get_hw_axis hw_axi_1] -address [format 0x%016lx [expr $addr +  8]] -type read
     create_hw_axi_txn -force r2 [get_hw_axis hw_axi_1] -address [format 0x%016lx [expr $addr + 16]] -type read
