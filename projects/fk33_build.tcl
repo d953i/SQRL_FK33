b023c4ebf770bc9c99f486a6f2a24966a4aa92fc
d9 <d9@idv-tech.com>
2019-09-09 21:25:35 -0400

    Add example build script with QOR flow

---
+++ projects/fk33_build.tcl
@@ -0,0 +1,11 @@
+
+synth_design -top bd_wrapper
+report_qor_suggestions -file post_synth_suggestions.rpt 
+opt_design 
+report_qor_suggestions -file post_opt_suggestions.rpt
+place_design
+report_qor_suggestions -file post_place_suggestions.rpt
+route_design
+report_qor_suggestions -file post_route_suggestions.rpt
+set_property ENABLED 1 [get_qor_suggestions]
+write_qor_suggestions all_enabled_suggestions.rqs
