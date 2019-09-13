00062508411d797c45f8ac4ed1ca59373eea8a60
d9 <d9@idv-tech.com>
2019-09-13 08:03:30 -0400

    Update fk33_build_qor.tcl

--- projects/fk33_build_qor.tcl
+++ projects/fk33_build_qor.tcl
@@ -1,12 +1,12 @@
 read_qor_suggestions all_enabled_suggestions.rqs
 
 synth_design -top bd_wrapper
-report_qor_suggestions -file post_synth_suggestions.rpt -force
+report_qor_suggestions -file post_synth_suggestions.rpt
 opt_design 
-report_qor_suggestions -file post_opt_suggestions.rpt -force
+report_qor_suggestions -file post_opt_suggestions.rpt
 place_design
-report_qor_suggestions -file post_place_suggestions.rpt -force
+report_qor_suggestions -file post_place_suggestions.rpt
 route_design
-report_qor_suggestions -file post_route_suggestions.rpt -force
+report_qor_suggestions -file post_route_suggestions.rpt
 set_property ENABLED 1 [get_qor_suggestions]
 write_qor_suggestions -force all_enabled_suggestions.rqs
